faa3e8e40915375705737e850b21b3da3aaa7701
Artem <kuvshinets@gmail.com>
2018-12-20 17:10:07 +0200

    create dataset

--- km84/Croucer_Artem/workshop5/source/dataset/creater.py
+++ km84/Croucer_Artem/workshop5/source/dataset/creater.py
@@ -1,5 +1,4 @@
 def create_dataset(file):
-
     dataset = dict()
     with open(file) as f:
         file_line = f.readline()
@@ -7,7 +6,8 @@ def create_dataset(file):
             return dataset
         file_line = f.readline()
         while file_line:
-            [minimum_age, maximum_age, gender, population, zipcode, geo_id] = [element.strip() for element in file_line.rstrip().split(",")]
+            [minimum_age, maximum_age, gender, population, zipcode, geo_id] = [element.strip() for element in
+                                                                               file_line.rstrip().split(",")]
             if geo_id not in dataset:
                 dataset[geo_id] = dict()
             if zipcode not in dataset[geo_id]:
@@ -21,4 +21,22 @@ def create_dataset(file):
     return dataset
 
 
-
+def create_dataset_4lines(file):
+    dataset = dict()
+    with open(file) as f:
+        file_line = f.readline()
+        if not file_line:
+            return dataset
+        file_line = f.readline()
+        while file_line:
+            [minimum_age, maximum_age, gender, population, zipcode, geo_id] = [element.strip() for element in
+                                                                                    file_line.rstrip().split(",")]
+            if minimum_age not in dataset:
+                dataset[minimum_age] = dict()
+            if maximum_age not in dataset[minimum_age]:
+                dataset[minimum_age][maximum_age] = dict()
+            dataset[minimum_age][maximum_age].update({
+                gender: {population}
+            })
+            file_line = f.readline()
+    return dataset
